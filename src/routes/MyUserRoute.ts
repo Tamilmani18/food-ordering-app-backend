aacfa09081c3c6d043fd34bc4d04faaace8d63ea
Tamil <Tamilmani.tm018@gmail.com>
2024-07-10 11:56:27 +0530

    deploy commit

---
+++ src/routes/MyUserRoute.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import MyUserController from "../controllers/MyUserController";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validation";
+
+const router = express.Router();
+
+// /api/my/user passes the post req to MyUserController
+
+// jwtCheck func is from oauth pkg which connects auth0 service based on the credentials we use to create account
+// & it validates if the auth token we get in the req is come from auth0 server
+router.get("/", jwtCheck, jwtParse, MyUserController.getCurrentUser);
+router.post("/", jwtCheck, MyUserController.createCurrentUser);
+router.put(
+  "/",
+  jwtCheck,
+  jwtParse,
+  validateMyUserRequest,
+  MyUserController.updateCurrentUser
+);
+
+export default router;
