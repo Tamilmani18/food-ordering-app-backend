51c829781ec44126ab456f990935d44862b768c6
Tamil <Tamilmani.tm018@gmail.com>
2024-08-18 13:58:18 +0530

    fixed free instance spin down inactivity

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import axios from "axios";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
 import restaurantRoute from "./routes/RestaurantRoute";
 import { v2 as cloudinary } from "cloudinary";
 import orderRoute from "./routes/OrderRoute";
-import axios from "axios";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -17,28 +17,6 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-const app = express();
-// middleware which converts body of any request to api server to json
-
-app.use(cors());
-
-app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
-
-app.use(express.json());
-
-app.get("/health", async (req: Request, res: Response) => {
-  res.send({ message: "Health ok !" });
-});
-
-app.use("/api/my/user", myUserRoute);
-app.use("/api/my/restaurant", myRestaurantRoute);
-app.use("/api/restaurant", restaurantRoute);
-app.use("/api/order", orderRoute);
-
-app.listen(7000, () => {
-  console.log("server started on localhost:7000");
 });
 
 const url = `https://food-ordering-app-backend-n0rv.onrender.com`;
@@ -64,3 +42,25 @@ function reloadWebsite() {
 }
 
 setInterval(reloadWebsite, interval);
+
+const app = express();
+// middleware which converts body of any request to api server to json
+
+app.use(cors());
+
+app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
+
+app.use(express.json());
+
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "Health ok !" });
+});
+
+app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
+app.use("/api/restaurant", restaurantRoute);
+app.use("/api/order", orderRoute);
+
+app.listen(7000, () => {
+  console.log("server started on localhost:7000");
+});
