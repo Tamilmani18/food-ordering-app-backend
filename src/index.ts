d4e32b31fe2ac9a7853d00b4bcf2f817f8c71936
Tamil <Tamilmani.tm018@gmail.com>
2024-09-01 17:50:14 +0530

    increased spin down inactive time

--- src/index.ts
+++ src/index.ts
@@ -43,7 +43,7 @@ app.listen(7000, () => {
 
 const url = `https://food-ordering-app-backend-n0rv.onrender.com/health`;
 
-const interval = 3600000;
+const interval = 1800000;
 
 function reloadWebsite() {
   axios
