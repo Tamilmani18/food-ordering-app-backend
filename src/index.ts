5bc1f05a850958a7c7d6e53d28a34285c3e2684a
Tamil <Tamilmani.tm018@gmail.com>
2024-10-14 09:51:56 +0530

    updated time interval

--- src/index.ts
+++ src/index.ts
@@ -46,21 +46,29 @@ const url = `https://food-ordering-app-backend-n0rv.onrender.com/health`;
 const interval = 90000;
 
 function reloadWebsite() {
-  axios
-    .get(url)
-    .then((response) => {
-      console.log(
-        `Reloaded at ${new Date().toISOString()}: Status Code ${
-          response.status
-        }`
-      );
-    })
-    .catch((error) => {
-      console.error(
-        `Error reloading at ${new Date().toISOString()}:`,
-        error.message
-      );
-    });
+  const now = new Date();
+  const currentHour = now.getHours();
+  if (currentHour >= 6 && currentHour < 18) {
+    axios
+      .get(url)
+      .then((response) => {
+        console.log(
+          `Reloaded at ${new Date().toISOString()}: Status Code ${
+            response.status
+          }`
+        );
+      })
+      .catch((error) => {
+        console.error(
+          `Error reloading at ${new Date().toISOString()}:`,
+          error.message
+        );
+      });
+  } else {
+    console.log(
+      `Skipping reload at ${new Date().toISOString()}: Outside daytime hours.`
+    );
+  }
 }
 
 setInterval(reloadWebsite, interval);
